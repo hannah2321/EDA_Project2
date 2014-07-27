78af09a0241f2b6b8c5509de5b420aac990669dd
hannah2321 <hannah2321@users.noreply.github.com>
2014-07-27 15:46:20 -0400

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,24 +1,22 @@
 # Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips==06037). Which city has seen greater changes over time in motor vehicle emissons?
 
-##Load the required libraries
+#Load packages
 library(plyr)
 library(ggplot2)
 
-## Read the data sets
+# Read data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-## subset
 SCC.motor <- grep("motor", SCC$Short.Name, ignore.case = TRUE)
 SCC.motor <- SCC[SCC.motor, ]
 SCC.identifiers <- as.character(SCC.motor$SCC)
 
-## subsetting
 NEI$SCC <- as.character(NEI$SCC)
 NEI.motor <- NEI[NEI$SCC %in% SCC.identifiers, ]
 NEI.motor.24510 <- NEI.motor[which(NEI.motor$fips == "24510"), ]
 
-## Build a plot
+# Create plot
 aggregate.motor.24510 <- with(NEI.motor.24510, aggregate(Emissions, by = list(year),sum))
 
 SCC.motor <- grep("motor", SCC$Short.Name, ignore.case = TRUE)
@@ -29,10 +27,10 @@ SCC.identifiers <- as.character(SCC.motor$SCC)
 NEI$SCC <- as.character(NEI$SCC)
 NEI.motor <- NEI[NEI$SCC %in% SCC.identifiers, ]
 
-## Baltimore
+# Baltimore
 NEI_motor_24510 <- NEI.motor[which(NEI.motor$fips == "24510"), ]
 
-## Los Angeles 
+# Los Angeles 
 NEI_motor_06037 <- NEI.motor[which(NEI.motor$fips == "06037"), ]
 
 aggregate_motor_24510 <- with(NEI_motor_24510, aggregate(Emissions, by = list(year),  sum))
@@ -40,13 +38,13 @@ aggregate_motor_24510$group <- rep("Baltimore County", length(aggregate_motor_24
 aggregate_motor_06037 <- with(NEI_motor_06037, aggregate(Emissions, by = list(year), sum))
 aggregate_motor_06037$group <- rep("Los Angeles County", length(aggregate_motor_06037[, 1]))
 
-## Aggregate the data
+# Aggregate data
 aggregated_motor_zips <- rbind(aggregate_motor_06037, aggregate_motor_24510)
 aggregated_motor_zips$group <- as.factor(aggregated_motor_zips$group)
 colnames(aggregated_motor_zips) <- c("Year", "Emissions", "Group")
 
 
-## Plot the graph
+# Plot graph
 png(filename='plot6.png')
 
 qplot(Year, Emissions, data = aggregated_motor_zips, group = Group, color = Group, 
